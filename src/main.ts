c60b899e60b9f777f3f25a16ab37a716a71de6d8
chengheai <842385139@qq.com>
2022-02-16 17:25:25 +0800

    add router4

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import routes from './router/index'
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
 const app = createApp(App)
 app.use(createPinia())
-
+app.use(router)
 app.mount('#app')
