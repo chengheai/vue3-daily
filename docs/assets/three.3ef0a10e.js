b4e237e6afd5432f163bb153041268a3d310910e
chengheai <842385139@qq.com>
2022-02-21 16:53:41 +0800

    feat: router

---
+++ docs/assets/three.3ef0a10e.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.fa0b64f4.js";import{o as r,c}from"./vendor.57f8dc82.js";const o={};function t(n,a){return r(),c("h1",null,"three Seciton")}var _=e(o,[["render",t]]);export{_ as default};
